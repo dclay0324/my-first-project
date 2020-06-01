d65d5aaa25af23b72c29cefb55efdf9801c107b4
dclay0324 <dclay0324@gmail.com>
2020-06-01 23:44:44 +0800

    Updates

---
+++ precache-manifest.52e4e74b2cb66cc3dcb361c8684b7966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d860d2334339245dd22de3dfc7702cb6",
+    "url": "/my-first-project/index.html"
+  },
+  {
+    "revision": "e2b789b3622015e4d9f0",
+    "url": "/my-first-project/static/css/main.3eb57161.chunk.css"
+  },
+  {
+    "revision": "d769469759ea40336092",
+    "url": "/my-first-project/static/js/2.e09c3038.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-project/static/js/2.e09c3038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b789b3622015e4d9f0",
+    "url": "/my-first-project/static/js/main.c3325b74.chunk.js"
+  },
+  {
+    "revision": "1e60f967f5b76c1c7ac1",
+    "url": "/my-first-project/static/js/runtime-main.9f2196c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
